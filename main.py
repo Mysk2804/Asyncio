8432d36275d498f21fd54ca82b3b4b563fafad4b
mysk2 <mysk2804@gmail.com>
2023-02-01 12:30:35 +1000

    3 Commit

--- main.py
+++ main.py
@@ -10,25 +10,29 @@ MAX_SIZE = 10
 
 async def paste_to_bd(people_list):
     async with Session() as session:
-        people_list_orm = [People(
-            id=item['id'],
-            name=item['name'],
-            height=item['height'],
-            mass=item['mass'],
-            hair_color=item['hair_color'],
-            skin_color=item['skin_color'],
-            eye_color=item['eye_color'],
-            birth_year=item['birth_year'],
-            gender=item['gender'],
-            homeworld=item['homeworld'],
-            films=item['films'],
-            starships=item['starships'],
-            vehicles=item['vehicles'],
-            species=item['species']
-        ) for item in people_list]
-        session.add_all(people_list_orm)
-        await session.commit()
-        print(people_list[-1].get('url'))
+        for item in people_list:
+            if item.get("title") == "None":
+                people_list_orm = [People(id=item["id"])]
+            else:
+                people_list_orm = [People(
+                    id=item['id'],
+                    name=item['name'],
+                    height=item['height'],
+                    mass=item['mass'],
+                    hair_color=item['hair_color'],
+                    skin_color=item['skin_color'],
+                    eye_color=item['eye_color'],
+                    birth_year=item['birth_year'],
+                    gender=item['gender'],
+                    homeworld=item['homeworld'],
+                    films=item['films'],
+                    starships=item['starships'],
+                    vehicles=item['vehicles'],
+                    species=item['species']
+                )]
+            session.add_all(people_list_orm)
+            await session.commit()
+            print(people_list[-1].get('url'))
 
 
 async def get_items(json_data, client, name):
@@ -43,21 +47,21 @@ async def get_items(json_data, client, name):
 
 async def get_people(people_id: int, client: ClientSession):
     url = f'https://swapi.dev/api/people/{people_id}/'
-    try:
-        async with client.get(url) as response:
-            json_data = await response.json()
-            json_data['id'] = people_id
-            json_data_world = await client.get(json_data['homeworld'])
-            json_data_world = await json_data_world.json()
-            json_data['homeworld'] = json_data_world['name']
-            json_data['vehicles'] = await get_items(json_data=json_data['vehicles'], client=client, name='name')
-            json_data['species'] = await get_items(json_data=json_data['species'], client=client, name='name')
-            json_data['films'] = await get_items(json_data=json_data['films'], client=client, name='title')
-            json_data['starships'] = await get_items(json_data=json_data['starships'], client=client, name='name')
+    async with client.get(url) as response:
+        json_data = await response.json()
+        if json_data.get("detail") == "Not found":
+            return {"title": "None", "id": people_id}
+        json_data['id'] = people_id
+        json_data_world = await client.get(json_data['homeworld'])
+        json_data_world = await json_data_world.json()
+        json_data['homeworld'] = json_data_world['name']
+        json_data['vehicles'] = await get_items(json_data=json_data['vehicles'], client=client, name='name')
+        json_data['species'] = await get_items(json_data=json_data['species'], client=client, name='name')
+        json_data['films'] = await get_items(json_data=json_data['films'], client=client, name='title')
+        json_data['starships'] = await get_items(json_data=json_data['starships'], client=client, name='name')
             # pprint(json_data)
-        return json_data
-    except Exception:
-        print('KeyError')
+    return json_data
+
 
 
 async def main():
